6df9ac3a0917ce162a82f9bcb54ade16883dff83
Alex <alexeymeshcheryakov68@gmail.com>
2023-03-05 23:11:30 +0300

    Added budget.py

---
+++ bookkeeper/view/budget.py
@@ -0,0 +1,23 @@
+from PySide6 import QtWidgets
+
+
+class BudgetWidget(QtWidgets.QTableWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        column_headers = "Сумма Бюджет".split()
+        row_headers = "День Неделя Месяц".split()
+
+        self.setColumnCount(len(column_headers))
+        self.setRowCount(len(row_headers))
+
+        self.setHorizontalHeaderLabels(column_headers)
+        self.setVerticalHeaderLabels(row_headers)
+
+        self.verticalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
+        self.verticalHeader().setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+        self.verticalHeader().setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
+
+        self.horizontalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
+        self.horizontalHeader().setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+
