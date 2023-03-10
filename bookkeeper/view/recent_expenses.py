5e711496cec6ce49ebe30486df04ed88ff6745db
Alex <alexeymeshcheryakov68@gmail.com>
2023-03-11 00:52:59 +0300

    add set_data

--- bookkeeper/view/recent_expenses.py
+++ bookkeeper/view/recent_expenses.py
@@ -1,56 +1,35 @@
 from PySide6 import QtWidgets
+from bookkeeper.models.expense import Expense
 
 
-class RecentExpensesTable(QtWidgets.QTableWidget):
+class RecentExpensesWidget(QtWidgets.QTableWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.setSelectionBehavior(QtWidgets.QTableWidget.SelectRows)
+        self.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
 
         column_headers = "Дата Сумма Категория Комментарий".split()
 
         self.setColumnCount(len(column_headers))
-        self.setRowCount(30)
+        # self.setRowCount(30)
+        self.setFixedHeight(300)
 
         self.setHorizontalHeaderLabels(column_headers)
         self.verticalHeader().hide()
 
         self.horizontalHeader().setSectionResizeMode(3, QtWidgets.QHeaderView.Stretch)
 
+    def set_data(self, data: list[Expense]):
+        self.setRowCount(0)
+        self.setRowCount(len(data))
 
-class RecentExpensesWidget(QtWidgets.QWidget):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        for i, expense in enumerate(data):
+            self.setItem(i, 0, QtWidgets.QTableWidgetItem(
+                expense.expense_date))
+            self.setItem(i, 1, QtWidgets.QTableWidgetItem(str(expense.amount)))
+            self.setItem(i, 2, QtWidgets.QTableWidgetItem(str(expense.category)))
+            self.setItem(i, 3, QtWidgets.QTableWidgetItem(expense.comment))
 
-        self.layout = QtWidgets.QVBoxLayout()
-        self.setLayout(self.layout)
 
-        self.expenses_table = RecentExpensesTable()
 
-        self.layout.addWidget(self.expenses_table)
-
-        self.del_button = QtWidgets.QPushButton("Удалить запись")
-
-        self.layout.addWidget(self.del_button)
-
-        self.del_button.clicked.connect(self.del_expense)
-
-        self.set_data([["2023", "10", "Конфеты", "Комментарий"],
-                       ["2022", "10", "Конфеты", "Комментарий"],
-                       ["2021", "10", "Конфеты", "Комментарий"],
-                       ["2020", "10", "Конфеты", "Комментарий"],
-                       ["2019", "10", "Конфеты", "Комментарий"],
-                       ["2018", "10", "Конфеты", "Комментарий"],
-                       ["2017", "10", "Конфеты", "Комментарий"]])
-
-    def del_expense(self):
-        self.expenses_table.removeRow(
-            self.expenses_table.currentRow()
-        )
-
-    def set_data(self, data):
-        for i, row in enumerate(data):
-            for j, x in enumerate(row):
-                self.expenses_table.setItem(
-                    i, j, QtWidgets.QTableWidgetItem(x)
-                )
