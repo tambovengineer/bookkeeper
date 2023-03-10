e10acde38bd6b179f67eedfc92a110bb5795f236
Alex <alexeymeshcheryakov68@gmail.com>
2023-03-11 00:56:04 +0300

    added Edit and Delete button

--- bookkeeper/view/add_expenses.py
+++ bookkeeper/view/add_expenses.py
@@ -10,24 +10,33 @@ class AddExpensesWidget(QtWidgets.QWidget):
         self.setLayout(self.layout)
 
         self.layout.addWidget(QtWidgets.QLabel("Сумма"), 0, 0)
-        self.layout.addWidget(QtWidgets.QLabel("Категория"), 1, 0)
+        self.layout.addWidget(QtWidgets.QLabel("Комментарий"), 1, 0)
+        self.layout.addWidget(QtWidgets.QLabel("Категория"), 2, 0)
 
         self.edit_box = QtWidgets.QLineEdit()
         self.edit_box.setPlaceholderText("0")
+
+        self.edit_comment_box = QtWidgets.QLineEdit()
+        self.edit_comment_box.setPlaceholderText("Комментарий")
 
         self.categories_list = ["Продукты", "Книги", "Транспорт"]
         self.categories_list_widget = QtWidgets.QComboBox()
         self.categories_list_widget.addItems(self.categories_list)
 
         self.add_button = QtWidgets.QPushButton("Добавить")
+        self.edit_expense_button = QtWidgets.QPushButton("Исправить")
+        self.del_button = QtWidgets.QPushButton("Удалить")
 
         self.edit_button = QtWidgets.QPushButton("Редактировать")
         self.edit_button.clicked.connect(self.edit_button_click)
 
         self.layout.addWidget(self.edit_box, 0, 1)
-        self.layout.addWidget(self.categories_list_widget, 1, 1)
-        self.layout.addWidget(self.add_button, 2, 1)
-        self.layout.addWidget(self.edit_button, 1, 2)
+        self.layout.addWidget(self.edit_comment_box, 1, 1)
+        self.layout.addWidget(self.categories_list_widget, 2, 1)
+        self.layout.addWidget(self.add_button, 3, 1)
+        self.layout.addWidget(self.edit_button, 2, 2)
+        self.layout.addWidget(self.edit_expense_button, 4, 1)
+        self.layout.addWidget(self.del_button, 5, 1)
 
     def edit_button_click(self):
         EditCategoriesWidget(self.categories_list).exec()
