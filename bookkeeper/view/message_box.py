edacce7358b6d699c9e80e8768d477741349a476
Alex <alexeymeshcheryakov68@gmail.com>
2023-03-11 00:52:01 +0300

    Added message_box.py

---
+++ bookkeeper/view/message_box.py
@@ -0,0 +1,20 @@
+from PySide6 import QtWidgets
+
+
+class MessageBox(QtWidgets.QMessageBox):
+    def __init__(self, window_title='window', message='message', is_critical=False, add_cancel=False):
+        super().__init__()
+
+        self.setWindowTitle(window_title)
+        self.setText(message)
+
+        if add_cancel:
+            self.button_flag = QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel
+        else:
+            self.button_flag = QtWidgets.QMessageBox.Ok
+        self.setStandardButtons(self.button_flag)
+
+        if is_critical:
+            self.setIcon(QtWidgets.QMessageBox.Critical)
+        else:
+            self.setIcon(QtWidgets.QMessageBox.Information)
